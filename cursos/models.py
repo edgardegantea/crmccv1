3c770a9782dcd5caab4a0a5e18e1498d270219f5
edgardegantea <edgar.degante.a@gmail.com>
2020-09-16 22:29:43 -0500

    Se agregó la app cursos

---
+++ cursos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Cursos(models.Model):
+    name = models.CharField(verbose_name='Nombre del curso:', unique=True, max_length=200)
+    description = models.TextField(verbose_name='Descripción del curso')
+    duration = models.PositiveSmallIntegerField(verbose_name='Duración en horas:')
+    url = models.URLField(verbose_name='URL del curso')
+
+    def __str__(self):
+        return "{0} -> {1}".format(self.duration, self.name)
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
